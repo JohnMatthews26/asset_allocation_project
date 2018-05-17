cec2e467521aa54346c53335f639003d10ed9ac8
John Matthews <johnmatthews2634@gmail.com>
2018-05-17 10:02:41 -0700

    add root.jsx

---
+++ frontend/root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class Root extends React.Component {
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Root/>, document.getElementById('main'));
+});
