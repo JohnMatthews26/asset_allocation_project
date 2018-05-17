2d8101d05d40bb4a512862ba1b0671e230ebfd36
John Matthews <johnmatthews2634@gmail.com>
2018-05-17 11:27:07 -0700

    slider functioning properly

--- bundle.js
+++ bundle.js
@@ -20000,6 +20000,8 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -20014,15 +20016,32 @@ var RiskInput = function (_React$Component) {
 
     var _this = _possibleConstructorReturn(this, (RiskInput.__proto__ || Object.getPrototypeOf(RiskInput)).call(this, props));
 
-    _this.state = {};
-
+    _this.state = {
+      risk_value: 5
+    };
+    _this.update = _this.update.bind(_this);
     return _this;
   }
 
   _createClass(RiskInput, [{
+    key: 'update',
+    value: function update(e) {
+      var _this2 = this;
+
+      return function (event) {
+        return _this2.setState(_defineProperty({}, e, event.target.value));
+      };
+    }
+  }, {
     key: 'render',
     value: function render() {
-      return _react2.default.createElement('div', { className: 'risk_slider' });
+      return _react2.default.createElement(
+        'div',
+        { className: 'risk_slider' },
+        _react2.default.createElement('input', { type: 'range', min: '1', max: '10',
+          value: this.state.risk_value, className: 'slider', onChange: this.update('risk_value') }),
+        this.state.risk_value
+      );
     }
   }]);
 
--- frontend/risk_input.jsx
+++ frontend/risk_input.jsx
@@ -6,9 +6,13 @@ class RiskInput extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      risk_value: 5
     };
+    this.update = this.update.bind(this);
+  }
 
+  update(e){
+    return event => this.setState({[e]: event.target.value});
   }
 
 
@@ -16,7 +20,9 @@ class RiskInput extends React.Component {
     return(
 
         <div className="risk_slider">
-
+          <input type="range" min="1" max="10"
+            value={this.state.risk_value} className="slider" onChange={this.update('risk_value')}/>
+          {this.state.risk_value}
         </div>
 
     );
